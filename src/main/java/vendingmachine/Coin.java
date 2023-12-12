0b121eac3124d9e18954195e29d0bc59dc766d95
HyungHoKim <hkim1109@naver.com>
2023-12-12 20:59:51 +0900

    feat(Coin): 금액을 동전으로 반환하는 기능 생성
    금액을 잔돈으로 반환하는 generateChangeCoins 생성

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,5 +1,12 @@
 package vendingmachine;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -12,5 +19,46 @@ public enum Coin {
         this.amount = amount;
     }
 
-    // 추가 기능 구현
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public static Map<Coin, Integer> generateChangeCoins(int money) {
+        Map<Coin, Integer> changeCoins = new EnumMap<>(Coin.class);
+        generateCoinMap(changeCoins);
+        List<Integer> coinAmounts = Arrays.stream(values())
+                .map(Coin::getAmount)
+                .collect(Collectors.toList());
+        putRandomCoins(money, changeCoins, coinAmounts);
+        return changeCoins;
+    }
+    
+    public static Coin getCoin(int money) {
+        return Arrays.stream(values())
+                .filter(coin -> coin.amount == money)
+                .findFirst()
+                .orElse(COIN_10);
+    }
+
+    private static void putRandomCoins(int money, Map<Coin, Integer> changeCoins, List<Integer> coinAmounts) {
+        while (money != 0) {
+            if (money < 50) {
+                changeCoins.replace(COIN_10, changeCoins.get(COIN_10) + money / 10);
+                break;
+            }
+            int randomAmount = Randoms.pickNumberInList(coinAmounts);
+            if (money - randomAmount >= 0) {
+                Coin randomCoin = getCoin(randomAmount);
+                changeCoins.replace(randomCoin, changeCoins.get(randomCoin) + 1);
+                money = money - randomAmount;
+            }
+        }
+    }
+
+    private static void generateCoinMap(Map<Coin, Integer> changeCoins) {
+        changeCoins.put(COIN_500, 0);
+        changeCoins.put(COIN_100, 0);
+        changeCoins.put(COIN_50, 0);
+        changeCoins.put(COIN_10, 0);
+    }
 }
